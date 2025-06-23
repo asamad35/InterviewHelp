8ff8ecbbffbf976bd3595c467747ef882bfcd48a
asamad35 <samad.abdus3535@gmail.com>
2025-06-24 03:07:14 +0530

    Added react-query and sonner for toast

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,5 +1,14 @@
+import { toast, Toaster } from 'sonner'
+import { QueryProvider } from './providers/query-provider'
+
 function App(): React.JSX.Element {
-  return <h1 className="text-3xl font-bold underline">Hello World</h1>
+  return (
+    <QueryProvider>
+      <Toaster richColors />
+      <h1 className="text-3xl font-bold underline">Hello World</h1>
+      <button onClick={() => toast.success('Hello')}>Click me</button>
+    </QueryProvider>
+  )
 }
 
 export default App
