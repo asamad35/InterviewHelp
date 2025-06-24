34c301bf672d79292c9ff3e9d8e79018beb2068c
asamad35 <samad.abdus3535@gmail.com>
2025-06-25 03:02:32 +0530

    added welcome screen and few packages

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,12 +1,21 @@
-import { toast, Toaster } from 'sonner'
+import { useCallback, useState } from 'react'
+import { Toaster } from 'sonner'
 import { QueryProvider } from './providers/query-provider'
+import { WelcomeScreen } from './components/welcome-screen'
 
 function App(): React.JSX.Element {
+  const [isSettingsOpen, setIsSettingsOpen] = useState(false)
+
+  const handleOpenSettings = useCallback(() => {
+    setIsSettingsOpen(true)
+  }, [])
+
   return (
     <QueryProvider>
       <Toaster richColors />
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-      <button onClick={() => toast.success('Hello')}>Click me</button>
+      <div className="relative">
+        <WelcomeScreen onOpenSettings={handleOpenSettings} />
+      </div>
     </QueryProvider>
   )
 }
